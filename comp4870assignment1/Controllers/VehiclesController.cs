3b0d9d87a00e87cacde1086728cce4d64e0d7bb6
linhng2210 <k.linh2210@gmail.com>
2024-03-27 01:27:09 -0700

    Reverted EditVehicle, added validations, displayed owner's email instead of Id

--- comp4870assignment1/Controllers/VehiclesController.cs
+++ comp4870assignment1/Controllers/VehiclesController.cs
@@ -80,6 +80,9 @@ namespace assignment1.Controllers;
             {
                 return NotFound();
             }
+            ViewData["MemberId"] = new SelectList(_context.Members
+            .Select(m => new { m.Id, m.UserName }), 
+            "Id", "UserName"); 
             return View(vehicle);
         }
 
@@ -115,6 +118,9 @@ namespace assignment1.Controllers;
                 }
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["MemberId"] = new SelectList(_context.Members
+            .Select(m => new { m.Id, m.UserName }), 
+            "Id", "UserName"); 
             return View(vehicle);
         }
 
